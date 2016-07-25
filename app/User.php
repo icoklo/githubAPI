498cf66864e4898fc884cb5b235e5bbd380d978b
icoklo <icoklo@foi.hr>
2016-07-25 09:34:07 +0200

    Napravljena tablica Grupa i pivot tablica
    Napravljene migracije za kreiranje tablice grupa i pivot tablice
    user_group te je kreiran model za tablicu Grupa

--- app/User.php
+++ app/User.php
@@ -23,4 +23,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function groups(){
+        return this->belongsToMany('Group');
+    }
 }
