2229587b9608b148da4a9045fc4d50f042048783
icoklo <icoklo@foi.hr>
2016-07-26 08:38:38 +0200

    Umjesto Route::auth napisane su stvarne rute
    Umjesto Route::auth napisane su stvarne rute koje Route::auth inace sam
    generira

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,10 +20,22 @@ Route::get('/', function () {
 
 Route::get('/test','GroupController@test');
 
+// Authentication Routes...
+Route::get('login', 'Auth\AuthController@showLoginForm');
+Route::post('login', 'Auth\AuthController@login');
+Route::get('logout', 'Auth\AuthController@logout');
+
+// Password Reset Routes...
+Route::get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
+Route::post('password/email', 'Auth\PasswordController@sendResetLinkEmail');
+Route::post('password/reset', 'Auth\PasswordController@reset');
+
 // grupa ruta na kojima se koriste middleware-i auth i check.role 
 Route::group(['middleware' => ['auth','check.role']], function(){
 
-	Route::auth();
+	// Registration Routes...
+	Route::get('register', 'Auth\AuthController@showRegistrationForm');
+	Route::post('register', 'Auth\AuthController@register');
 
 	Route::post('/group','GroupController@createGroup'); // create group
 
